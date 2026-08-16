66ba7dd7f0510cbb07cc98671e367f5e494e3cb7
Chu-Xin-r <1356607455@qq.com>
2026-08-17 01:03:42 +0800

    chore: remove temp diagnostic scripts

--- del_lnks.ps1
+++
@@ -1,8 +0,0 @@
-$dir = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
-Write-Output ('DIR: ' + $dir)
-$before = (Get-ChildItem $dir -Force | Select-Object -ExpandProperty Name) -join ', '
-Write-Output ('BEFORE: ' + $before)
-Get-ChildItem $dir -Filter 'EasyTier.bat*' -Force | Remove-Item -Force
-Get-ChildItem $dir -Filter 'mediamtx.bat*' -Force | Remove-Item -Force
-$after = (Get-ChildItem $dir -Force | Select-Object -ExpandProperty Name) -join ', '
-Write-Output ('AFTER: ' + $after)
